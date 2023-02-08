0685edde7581098f2acd194ba5ccfa52b39a9733
11701 <logan.peek@vtsd.com>
2023-02-08 09:51:18 -0500

    2/8/23

--- Main.java
+++ Main.java
@@ -2,41 +2,66 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String mHistory[] = new String[6];
         String gHistory[] = new String[6];
 
-        for(int k = 0; k <= 1; k++){
+        for (int k = 0; k <= 5; k++) {
 
             Scanner muscles = new Scanner(System.in);
             System.out.println("Please Write Bellow Your Muscle Group of Choice.");
             System.out.print("Muscle Group: ");
             String Muscles = muscles.nextLine();
+            switch (Muscles) {
+                case "Back": Workouts.back();break;
+                case "back": Workouts.back();break;
+                case "Chest": Workouts.chest();break;
+                case "chest": Workouts.chest();break;
+                case "Legs": Workouts.legs();break;
+                case "legs": Workouts.legs();break;
+                case "Arms": Workouts.arms();break;
+                case "arms": Workouts.arms();break;
+                case "Shoulders": Workouts.shoulders();break;
+                case "shoulders": Workouts.shoulders();break;
+                default:
+                    System.out.print("I dont think that's a body part...");
+                    Scanner mistakemuscles = new Scanner(System.in);
+                    System.out.println("Please Write Bellow Your Actual Muscle Group of Choice.");
+                    System.out.print("Muscle Group: ");
+                    Muscles = mistakemuscles.nextLine();
+                    System.out.println();
+                    break;
+            }
             System.out.println();
-            //mHistory[k] = Muscles;
+            mHistory[k] = Muscles;
 
             Scanner goal = new Scanner(System.in);
-            System.out.println("Now Please Select and Write Your Exercise Goal (Strength, Cardio, Athletic)");
+            System.out.println("Now Please Select and Write Your Exercise Goal (Strength, Conditioning, Athletic)");
             System.out.print("Goal: ");
             String Goal = goal.nextLine();
-            //gHistory[k] = Goal;
-
-
-            MusclesGoals.Muscles(Muscles);
-            MusclesGoals.Goals(Goal);
-
-            mHistory[k] = Muscles;
+            switch (Goal) {
+                case "Strength": SetsReps.strength();break;
+                case "strength": SetsReps.strength();break;
+                case "Conditioning": SetsReps.cardio();break;
+                case "conditioning": SetsReps.cardio();break;
+                case "Athletic": SetsReps.athletic();break;
+                case "athletic": SetsReps.athletic();break;
+                default:
+                    System.out.print("Maybe try that again...");
+                    Scanner mistakegoal = new Scanner(System.in);
+                    System.out.println("Now Please Select and Write Your Actual Exercise Goal (Strength, Cardio, Athletic)");
+                    System.out.print("Goal: ");
+                    Goal = mistakegoal.nextLine();
+                    break;
+            }
             gHistory[k] = Goal;
-
         }
         System.out.println("Good Job this week! Take a look back on what you did this week and relax.");
-        for(int k = 0; k <= 1; k++) {
+        for (int k = 0; k <= 5; k++) {
             int day = k + 1;
             System.out.println("Day " + day + ": " + mHistory[k] + " with the goal of " + gHistory[k]);
         }
         System.out.println("See You Next Week!");
     }
-
-
 }
